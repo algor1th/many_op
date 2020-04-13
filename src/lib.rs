16dae3d02e994da399981aa23fdb3d9f0e76d876
Lenny Händler <lenny@lennyhaendler.com>
2020-04-13 02:18:54 +0200

    Api change: need semicolon behing every statement variable
    also updated tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,62 +4,68 @@
 /// Negate all passed variables
 /// # Example
 ///  ```
-/// #[test]
-/// fn it_works() {
+/// use many_op::{negate,do_for};
 /// let mut testvals = [true, false, true];
 /// negate! {
 ///     testvals[0];
 ///     testvals[2];
 /// };
 /// assert_eq!(testvals, [false, false, false]);
-/// }
 /// ```
 macro_rules! negate {
-    ($($e:expr);*) => {
-        do_for!(unary !, $($e);*)
+    ($($e:expr;)*) => {
+        do_for!(unary !, $($e;)*)
     };
 }
 #[macro_export]
 /// Do an operation for all following variables
 /// # Examples
 ///  ```
-/// #[test]
-/// fn it_works() {
+/// use many_op::do_for;
 /// let mut testvals = [true, true, true];
-/// do_for! {unary !
+/// do_for! {unary !,
 ///     testvals[0];
 ///     testvals[2];
 /// };
 /// assert_eq!(testvals, [false, true, false]);
-/// }
 /// ```
 /// ```
-/// #[test]
-/// fn it_works2() {
+/// use many_op::do_for;
 /// let mut testvals = [true, false, true];
-/// do_for! {binary ^, testvals[0];testvals[2]};
+/// do_for! {binary ^, testvals[0];testvals[2];};
 /// assert_eq!(testvals, [false, false, false]);
-/// }
 /// ```
 /// ```
-/// #[test]
-/// fn it_works3() {
+/// use many_op::do_for;
 /// let mut testvals = [1, 2, 3];
-/// do_for! {assign +=, testvals[0]; testvals[2]}
+/// do_for! {assign +=, testvals[0]; testvals[2];}
 /// assert_eq!(testvals, [2, 2, 6])
-/// }
+/// ```
+/// ```
+/// use many_op::do_for;
+/// let mut testvals = [1, 2, 3];
+/// do_for! {binary +, testvals[0] => testvals[1]; testvals[2] => testvals[0];}
+/// assert_eq!(testvals, [3, 2, 6])
 /// ```
 macro_rules! do_for {
-    (binary $op:tt, $($i:expr);*) => {
+    (binary $op:tt, $($i:expr;)*) => {
         $($i = $i $op $i);*
     };
-    (binary $op:tt, $($i:expr => $e:expr);*) => {
+    (binary $op:tt, $($i:expr => $e:expr;)*) => {
         $($i = $i $op $e);*
     };
-    (unary $op:tt, $($i:expr);*) => {
+    (unary $op:tt, $($i:expr;)*) => {
         $($i = $op $i);*
     };
-    (assign $a_op:tt, $($i:expr);*) => {
+    (assign $a_op:tt, $($i:expr;)*) => {
         $($i $a_op $i);*
     };
+}
+mod tests {
+    #[test]
+    fn binary_two_ops() {
+        let mut testvals = [1, 2, 3];
+        do_for! {binary +, testvals[0] => testvals[1]; testvals[2] => testvals[0];}
+        assert_eq!(testvals, [3, 2, 6])
+    }
 }
